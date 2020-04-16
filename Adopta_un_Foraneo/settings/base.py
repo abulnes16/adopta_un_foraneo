4c574d71893872c1a2f3a56e392e625ddb588b80
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-04-15 20:05:53 -0600

    Agregado whitenoise

--- Adopta_un_Foraneo/settings/base.py
+++ Adopta_un_Foraneo/settings/base.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Adopta_un_Foraneo.urls'
@@ -109,6 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
